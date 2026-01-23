23f6a24cbcf9b97adbfc9fba5f7afc6c28fe026b
Hussein Bani Khaled <hansonhans88@gmail.com>
2026-01-23 14:58:05 -0500

    adding user model and userrepo and passwordencodeer && add spring secuirty dependency

---
+++ src/main/java/ca/hanson/shiftflow_backend/entitiy/Role.java
@@ -0,0 +1,14 @@
+package ca.hanson.shiftflow_backend.entitiy;
+
+
+
+
+
+public enum Role {
+
+    ADMIN,
+    MANAGER,
+    EMPLOYEE
+
+
+}
---
+++ src/main/java/ca/hanson/shiftflow_backend/entitiy/User.java
@@ -0,0 +1,37 @@
+package ca.hanson.shiftflow_backend.entitiy;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime createdAt;
+
+
+
+}
