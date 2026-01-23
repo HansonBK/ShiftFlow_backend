5cde98f3e9ff84168864c044f7c6a9e80fe74d14
Hussein Bani Khaled <hansonhans88@gmail.com>
2026-01-23 16:20:41 -0500

    generate jwt token

--- src/main/java/ca/hanson/shiftflow_backend/security/JwtTokenProvider.java
+++ src/main/java/ca/hanson/shiftflow_backend/security/JwtTokenProvider.java
@@ -1,8 +1,16 @@
 package ca.hanson.shiftflow_backend.security;
 
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtTokenProvider {
@@ -10,7 +18,34 @@ public class JwtTokenProvider {
     @Value("${jwt.secret}")
     private String jwtSecret;
     @Value("${jwt.expiration-ms}")
-    private String jwtExpirationTime;
+    private long jwtExpirationTime;
+
+    public String generateToken(String email, String role) {
+
+        Map<String, Object> claims = new HashMap<>();
+
+        claims.put("role", role);
+        return createToken(claims, email);
+    }
+
+    private String createToken(Map<String, Object> claims, String email) {
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationTime);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+
+
+
 
 
 
