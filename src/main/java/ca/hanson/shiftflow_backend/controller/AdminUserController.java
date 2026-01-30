969d8a762da23cbfe6f92c087682334fbc9d13a1
Hussein Bani Khaled <hansonhans88@gmail.com>
2026-01-30 13:51:49 -0500

    added the getall users for admin

--- src/main/java/ca/hanson/shiftflow_backend/controller/AdminUserController.java
+++ src/main/java/ca/hanson/shiftflow_backend/controller/AdminUserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController()
 @RequestMapping("/api/admin/users")
 public class AdminUserController {
@@ -30,6 +32,12 @@ public class AdminUserController {
         adminUserService.deleteUser(id);
         return ResponseEntity.noContent().build();
 
+    }
+
+    @GetMapping()
+    public List<User> getAllUsers() {
+
+        return adminUserService.getAllUsers();
     }
 
 }
