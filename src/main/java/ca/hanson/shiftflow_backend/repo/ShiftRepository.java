64a5a047f7ec6f13a1bf8fc749f8cc26c4acbdde
Hussein Bani Khaled <hansonhans88@gmail.com>
2026-02-03 12:40:23 -0500

    Polish core shift features

--- src/main/java/ca/hanson/shiftflow_backend/repo/ShiftRepository.java
+++ src/main/java/ca/hanson/shiftflow_backend/repo/ShiftRepository.java
@@ -5,10 +5,21 @@ import ca.hanson.shiftflow_backend.entitiy.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
 
+    List<Shift> findByAssignedEmployeeId(Long assignedEmployeeId);
 
+    List<Shift> findByAssignedEmployeeEmail(String email);
+
+    List<Shift> findByAssignedEmployeeIdAndCreatedByEmailOrderByStartTimeAsc(
+            Long assignedEmployeeId,
+            String managerEmail
+    );
+
+    List<Shift> findByAssignedEmployeeEmailOrderByStartTimeAsc(String email);
 
 }
